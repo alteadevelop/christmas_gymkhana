99dc0d2f8dfbd0dd0d00ad996143a46130e721bd
alteadev <alteadev@gmail.com>
2021-12-14 22:24:15 +0100

    init project

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="mainDiv">
+    <HelloApp msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloApp from "./components/HelloApp.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloApp,
+  },
+};
+</script>
+
+<style>
+
+@import url('https://fonts.googleapis.com/css2?family=Bakbak+One&display=swap');
+
+.mainDiv {
+  font-family: 'Bakbak One', cursive;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+  background-image: url(./assets/background_image.jpg);
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+}
+</style>
---
+++ src/components/HelloApp.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Christmas Gymkhana</h1>
+    <div class="rules-app">
+      <p>Bienvenida a la Gymkhana navideña. En cada nivel, en caso de acertarle, recibirás tus regalos navideños. Mucha suerte.</p>
+      <button>Comenzar</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloApp'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+div{
+    background-image: url('/assets/background_image.jpg');
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    height: 100%;
+    width: 100%;
+    text-align: center;
+}
+
+
+</style>
